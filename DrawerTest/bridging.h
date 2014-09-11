c1225d5a97c5800568e737a9493e6a99bd551dd2
Tom Kidd <tkidd@capis.com>
2014-09-10 22:46:27 -0500

    Initial setup - storyboard mimics image on site

---
+++ DrawerTest/bridging.h
@@ -0,0 +1,14 @@
+//
+//  bridging.h
+//  DrawerTest
+//
+//  Created by Tom Kidd on 9/10/14.
+//  Copyright (c) 2014 Tom Kidd. All rights reserved.
+//
+
+#ifndef DrawerTest_bridging_h
+#define DrawerTest_bridging_h
+
+#import "MMDrawerController.h"
+
+#endif
--- DrawerTest/rootViewController.swift
+++ DrawerTest/rootViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class rootViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
