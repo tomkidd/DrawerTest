a75a90771e0cf130331f72147a039dfdc4b1bf01
Tom Kidd <tkidd@capis.com>
2014-09-11 00:39:18 -0500

    got sub-side menus working

--- DrawerTest/SideDrawerViewController.swift
+++ DrawerTest/SideDrawerViewController.swift
@@ -12,17 +12,27 @@ class SideDrawerViewController: UIViewController {
     
     @IBOutlet var FirstVC:UIButton?
     @IBOutlet var SecondVC:UIButton?
+    @IBOutlet var ThirdVC:UIButton?
     
     //[self.mm_drawerController setCenterViewController:[self.storyboard instantiateViewControllerWithIdentifier:@"SECOND_TOP_VIEW_CONTROLLER"] withCloseAnimation:YES completion:nil];
     
     @IBAction func goFirstVC(x:UIButton) {
-        self.mm_drawerController.setCenterViewController(self.storyboard!.instantiateViewControllerWithIdentifier("FIRST_TOP_VIEW_CONTROLLER") as UIViewController, withCloseAnimation: true, completion: nil)
+        self.mm_drawerController.setCenterViewController(self.storyboard!.instantiateViewControllerWithIdentifier("FIRST_TOP_VIEW_CONTROLLER") as UIViewController, withCloseAnimation: true, completion:{finished in self.mm_drawerController.leftDrawerViewController = self.storyboard!.instantiateViewControllerWithIdentifier("SIDE_DRAWER_CONTROLLER") as UIViewController})
     }
     
     @IBAction func goSecondVC(x:UIButton) {
-        self.mm_drawerController.setCenterViewController(self.storyboard!.instantiateViewControllerWithIdentifier("SECOND_TOP_VIEW_CONTROLLER") as UIViewController, withCloseAnimation: true, completion: nil)
+        self.mm_drawerController.setCenterViewController(self.storyboard!.instantiateViewControllerWithIdentifier("SECOND_TOP_VIEW_CONTROLLER") as UIViewController, withCloseAnimation: true, completion: {finished in self.mm_drawerController.leftDrawerViewController = self.storyboard!.instantiateViewControllerWithIdentifier("SIDE_DRAWER_CONTROLLER") as UIViewController})
     }
-
+    
+    @IBAction func goThirdVC(x:UIButton) {
+        self.mm_drawerController.leftDrawerViewController = self.storyboard!.instantiateViewControllerWithIdentifier("SIDE_DRAWER_CONTROLLER_2") as UIViewController
+    }
+    
+    func resetmenu() -> Bool {
+        self.mm_drawerController.leftDrawerViewController = self.storyboard!.instantiateViewControllerWithIdentifier("SIDE_DRAWER_CONTROLLER") as UIViewController
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
